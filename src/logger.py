b553d7225f3d4d028fb355281afd866f89d74386
vijayaragavaan2065 <22cb063@kpriet.ac.in>
2025-04-22 09:08:49 +0530

    Refactored component filenames and updated logger and exception modules

---
+++ src/logger.py
@@ -0,0 +1,26 @@
+import logging
+import os
+from datetime import datetime
+
+# Go one level up from src/ to MlProject/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Generate log file name
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Create logs directory inside the project root
+logs_path = os.path.join(BASE_DIR, "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+# Final log file path
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Set up logging configuration
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
